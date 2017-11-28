f101e03693a97c0005d7c0eefed31f6b45e1bb98
Erven,Bart B.J.P.R <b.erven@student.fontys.nl>
2017-11-29 00:56:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return '''
+    <pre>
+    m    m        ""#    ""#                 m     m               ""#        #
+    #    #  mmm     #      #     mmm         #  #  #  mmm    m mm    #     mmm#
+    #mmmm# #"  #    #      #    #" "#        " #"# # #" "#   #"  "   #    #" "#
+    #    # #""""    #      #    #   #         ## ##" #   #   #       #    #   #
+    #    # "#mm"    "mm    "mm  "#m#"         #   #  "#m#"   #       "mm  "#m##
+ 
+    Dit is python in een docker container!
+ 
+    * Dependency 'flask' is geinstalleerd
+    * Port 5000 is opengezet in de container
+    </pre>
+    '''
+
 @app.route('/test')
 def test():
     return 'Oii'
